039a065cf5a1f615366b3ce5f629e827695a55dc
JahidKhan67 <mdzahidkhanjoy@gmail.com>
2023-04-10 06:13:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Components/Header';
+
+const App = () => {
+  return (
+    <div>
+      {/* Header */}
+      <Header></Header>
+      {/* Header */}
+      <Outlet></Outlet>
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default App;
